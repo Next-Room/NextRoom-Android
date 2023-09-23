551a184a8bf155e4905e1178aa7045e6179e7e18
mangbaam <pmb0836@gmail.com>
2023-09-23 18:30:01 +0900

    #8 입력창 커스텀 TextField -> BasicTextField 사용

--- presentation/src/main/java/com/nexters/nextroom/presentation/ui/component/textinput/NRTextInput.kt
+++ presentation/src/main/java/com/nexters/nextroom/presentation/ui/component/textinput/NRTextInput.kt
@@ -1,13 +1,16 @@
 package com.nexters.nextroom.presentation.ui.component.textinput
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,11 +19,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nexters.nextroom.presentation.ui.theme.Gray01
 import com.nexters.nextroom.presentation.ui.theme.NextRoomTheme
@@ -28,55 +33,86 @@ import com.nexters.nextroom.presentation.ui.theme.Red
 import com.nexters.nextroom.presentation.ui.theme.TextInput
 import com.nexters.nextroom.presentation.ui.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NRTextInput(
     modifier: Modifier = Modifier,
     value: String,
     hint: String? = null,
+    enabled: Boolean = true,
     isError: Boolean = false,
     isPassword: Boolean = false,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     onValueChange: (String) -> Unit,
 ) {
-    TextField(
-        modifier = modifier.wrapContentHeight(),
+    val colors = TextFieldDefaults.colors(
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        errorContainerColor = Color.Transparent,
+        focusedIndicatorColor = White,
+        unfocusedIndicatorColor = Gray01,
+        errorIndicatorColor = Red,
+        unfocusedTextColor = White,
+        focusedTextColor = White,
+        errorTextColor = White,
+    )
+    val visualTransformation = if (isPassword) {
+        PasswordVisualTransformation('●')
+    } else {
+        VisualTransformation.None
+    }
+
+    val textStyle = MaterialTheme.typography.TextInput
+    val textColor = textStyle.color.takeOrElse {
+        when {
+            isError -> Red
+            !enabled -> Gray01
+            else -> White
+        }
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    BasicTextField(
         value = value,
+        modifier = modifier.wrapContentHeight(),
         onValueChange = onValueChange,
-        placeholder = hint?.let {
-            {
-                Text(
-                    text = hint,
-                    color = Gray01,
-                    fontSize = 16.sp,
-                )
-            }
-        },
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent,
-            errorContainerColor = Color.Transparent,
-            focusedIndicatorColor = White,
-            unfocusedIndicatorColor = Gray01,
-            errorIndicatorColor = Red,
-            unfocusedTextColor = Gray01,
-            focusedTextColor = White,
-            errorTextColor = White,
-        ),
-        textStyle = MaterialTheme.typography.TextInput,
-        isError = isError,
+        enabled = enabled,
+        readOnly = false,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
-        visualTransformation = if (isPassword) {
-            PasswordVisualTransformation('●')
-        } else {
-            VisualTransformation.None
-        },
+        textStyle = mergedTextStyle,
+        cursorBrush = SolidColor(White),
+        visualTransformation = visualTransformation,
         singleLine = true,
+        interactionSource = interactionSource,
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                enabled = enabled,
+                isError = isError,
+                singleLine = true,
+                contentPadding = PaddingValues(start = 4.dp, end = 4.dp, bottom = 8.dp),
+                interactionSource = interactionSource,
+                colors = colors,
+                placeholder = {
+                    hint?.let {
+                        Text(
+                            text = hint,
+                            color = Gray01,
+                            fontSize = 16.sp,
+                        )
+                    }
+                },
+            )
+        },
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview
 @Composable
 fun NRTextInputPreview() {
     NextRoomTheme {
@@ -86,7 +122,7 @@ fun NRTextInputPreview() {
                 value = text,
                 onValueChange = { text = it },
                 isError = text == "error",
-                isPassword = true,
+                isPassword = false,
             )
         }
     }
