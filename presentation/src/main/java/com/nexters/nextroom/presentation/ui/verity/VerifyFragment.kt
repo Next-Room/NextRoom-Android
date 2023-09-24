c4d375839bc4ddabe35b89d805db36fb29405fd1
mangbaam <pmb0836@gmail.com>
2023-09-24 19:21:47 +0900

    #9 관리자 코드 입력 창 숫자 타입으로 변경

--- presentation/src/main/java/com/nexters/nextroom/presentation/ui/verity/VerifyFragment.kt
+++ presentation/src/main/java/com/nexters/nextroom/presentation/ui/verity/VerifyFragment.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -103,7 +104,7 @@ class VerifyFragment :
                 value = state.currentInput,
                 onValueChange = viewModel::inputCode,
                 hint = stringResource(id = R.string.admin_code_hint),
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done, keyboardType = KeyboardType.Decimal),
                 keyboardActions = KeyboardActions(onDone = { viewModel.complete() }),
             )
             MainButton(
