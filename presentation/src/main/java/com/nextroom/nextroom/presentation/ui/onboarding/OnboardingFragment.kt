b59e62824436feee687250c51fd9d4340ad435cc
mangbaam <pmb0836@gmail.com>
2023-10-27 17:15:44 +0900

    #27 refactor: OnboardingFragment 가 BaseFragment 상속하도록 수정

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/OnboardingFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/OnboardingFragment.kt
@@ -1,29 +1,15 @@
 package com.nextroom.nextroom.presentation.ui.onboarding
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.nextroom.nextroom.presentation.base.BaseFragment
 import com.nextroom.nextroom.presentation.databinding.FragmentOnboardingBinding
 import com.nextroom.nextroom.presentation.extension.safeNavigate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OnboardingFragment : Fragment() {
-    private var _binding: FragmentOnboardingBinding? = null
-    private val binding
-        get() = _binding ?: error("binding is null")
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>(FragmentOnboardingBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -33,7 +19,8 @@ class OnboardingFragment : Fragment() {
 
     private fun initViews() {
         binding.tvApplyFreeTrial.setOnClickListener {
-            val action = OnboardingFragmentDirections.actionGlobalWebViewFragment("https://m.naver.com/") // TODO: 홈페이지 주소 확정시 변경
+            val action =
+                OnboardingFragmentDirections.actionGlobalWebViewFragment("https://m.naver.com/") // TODO: 홈페이지 주소 확정시 변경
             findNavController().safeNavigate(action)
         }
         binding.tvExistingUserGuide.setOnClickListener {
