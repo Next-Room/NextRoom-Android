4468f5eea99844fcc57a1df0bfd22ae960d898b5
mangbaam <pmb0836@gmail.com>
2023-11-18 21:43:02 +0900

    #42 feat: 통계 DB Entity, Dao 구축

---
+++ data/src/main/java/com/nextroom/nextroom/data/repository/StatisticsRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.nextroom.nextroom.data.repository
+
+import com.nextroom.nextroom.data.datasource.StatisticsDataSource
+import com.nextroom.nextroom.domain.model.statistics.GameStats
+import com.nextroom.nextroom.domain.model.statistics.HintStats
+import com.nextroom.nextroom.domain.repository.StatisticsRepository
+import javax.inject.Inject
+
+class StatisticsRepositoryImpl @Inject constructor(
+    private val dataSource: StatisticsDataSource,
+) : StatisticsRepository {
+
+    override suspend fun recordGameStartStats(gameStats: GameStats) {
+        dataSource.recordGameStartStats(gameStats)
+    }
+
+    override suspend fun recordHintStats(hintStats: HintStats) {
+        dataSource.recordHintStats(hintStats)
+    }
+
+    override suspend fun postGameStats() {
+        dataSource.postGameStats()
+    }
+}
