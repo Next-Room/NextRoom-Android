79186cc657ac673bebc5790426c528ce1616184f
mangbaam <pmb0836@gmail.com>
2024-04-07 23:39:31 +0900

    #70 feat: 관리자 코드 입력 안했거나 잘못된 경우 에러 메시지 노출

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/verity/VerifyViewModel.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/verity/VerifyViewModel.kt
@@ -1,22 +1,13 @@
 package com.nextroom.nextroom.presentation.ui.verity
 
 import androidx.annotation.StringRes
-import androidx.lifecycle.viewModelScope
-import com.nextroom.nextroom.domain.model.onSuccess
 import com.nextroom.nextroom.domain.repository.AdminRepository
-import com.nextroom.nextroom.domain.repository.GameStateRepository
-import com.nextroom.nextroom.domain.repository.ThemeRepository
 import com.nextroom.nextroom.presentation.R
 import com.nextroom.nextroom.presentation.base.BaseViewModel
 import com.nextroom.nextroom.presentation.model.InputState
-import com.nextroom.nextroom.presentation.model.ThemeInfoPresentation
 import com.nextroom.nextroom.presentation.model.UiText
-import com.nextroom.nextroom.presentation.model.toPresentation
 import com.nextroom.nextroom.presentation.ui.login.LoginEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
@@ -27,25 +18,8 @@ import javax.inject.Inject
 @HiltViewModel
 class VerifyViewModel @Inject constructor(
     private val adminRepository: AdminRepository,
-    private val themeRepository: ThemeRepository,
-    private val gameStateRepository: GameStateRepository,
-) : BaseViewModel<VerifyState, LoginEvent>() { // TODO: LoginEvent 사용 하지 말고 따로 Event 분리 하기
+) : BaseViewModel<VerifyState, LoginEvent>() {
     override val container: Container<VerifyState, LoginEvent> = container(VerifyState())
-
-    private val themes = MutableStateFlow<List<ThemeInfoPresentation>>(emptyList())
-    val _themes = themes.asStateFlow()
-
-    /*init {
-        fetchThemes()
-    }
-
-    private fun fetchThemes() {
-        viewModelScope.launch {
-            themeRepository.getThemes().onSuccess {
-                themes.emit(it.map { it.toPresentation(System.currentTimeMillis()) })
-            }
-        }
-    }*/
 
     fun inputCode(code: String) = intent {
         reduce {
@@ -56,33 +30,20 @@ class VerifyViewModel @Inject constructor(
         }
     }
 
-    fun forgotCode() = intent {
-        showMessage("현재 지원되지 않는 기능입니다")
-    }
-
     fun complete() = intent {
+        if (state.currentInput.isBlank()) {
+            showMessage(R.string.admin_code_required)
+            return@intent
+        }
+
         val success = adminRepository.verifyAdminCode(state.currentInput)
         reduce {
             state.copy(inputState = if (success) InputState.Ok else InputState.Error(R.string.blank))
         }
-    }
-
-    /*fun startGame(themeId: Int) = intent {
-        viewModelScope.launch {
-            themes
-                .value
-                .find { it.id == themeId }
-                ?.also {
-                    gameStateRepository.saveGameState(
-                        timeLimitInMinute = it.timeLimit,
-                        hintLimit = it.hintLimit,
-                        usedHints = emptySet(),
-                        startTime = System.currentTimeMillis(),
-                    )
-                    postSideEffect(LoginEvent.GoToMainScreen)
-                }
+        if (!success) {
+            showMessage(R.string.admin_code_invalid)
         }
-    }*/
+    }
 
     private fun showMessage(message: String) = intent {
         postSideEffect(LoginEvent.ShowMessage(UiText(message)))
