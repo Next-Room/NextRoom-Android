764bc2a9c7f1c66acea9c45a5c759e4c9b67d894
mangbaam <pmb0836@gmail.com>
2024-04-21 21:56:17 +0900

    #73 feat: 회원가입 URL 연결

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/OnboardingFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/OnboardingFragment.kt
@@ -3,6 +3,7 @@ package com.nextroom.nextroom.presentation.ui.onboarding
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.nextroom.nextroom.presentation.BuildConfig
 import com.nextroom.nextroom.presentation.base.BaseFragment
 import com.nextroom.nextroom.presentation.databinding.FragmentOnboardingBinding
 import com.nextroom.nextroom.presentation.extension.safeNavigate
@@ -19,8 +20,9 @@ class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>(FragmentOnboa
 
     private fun initViews() {
         binding.tvApplyFreeTrial.setOnClickListener {
+            val url = if (BuildConfig.DEBUG) "https://dev.nextroom.co.kr/signup" else "https://nextroom.co.kr/signup"
             val action =
-                OnboardingFragmentDirections.actionGlobalWebViewFragment("https://nextroom.co.kr/")
+                OnboardingFragmentDirections.actionGlobalWebViewFragment(url)
             findNavController().safeNavigate(action)
         }
         binding.tvExistingUserGuide.setOnClickListener {
