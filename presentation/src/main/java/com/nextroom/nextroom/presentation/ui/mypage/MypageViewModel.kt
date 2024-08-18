6df9e7cca9d0f29cbba9d142a2dc65f95b6a810a
juhwankim-dev <juhwan.dev@gmail.com>
2024-08-18 19:02:56 +0900

    #89 feat: 탈퇴하기 로직 수행시 로딩 추가

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/mypage/MypageViewModel.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/mypage/MypageViewModel.kt
@@ -4,13 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nextroom.nextroom.domain.model.SubscribeStatus
 import com.nextroom.nextroom.domain.model.onFailure
+import com.nextroom.nextroom.domain.model.onFinally
 import com.nextroom.nextroom.domain.model.onSuccess
 import com.nextroom.nextroom.domain.repository.AdminRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,8 +22,19 @@ class MypageViewModel @Inject constructor(
     private val adminRepository: AdminRepository,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
-    val uiState = _uiState.asStateFlow()
+    private val _myInfo = MutableStateFlow<UiState>(UiState.Loading)
+    private val _isResignLoading = MutableStateFlow(false)
+
+    val uiState = combine(
+        _myInfo,
+        _isResignLoading,
+    ) { myInfo, isResignLoading ->
+        if (myInfo is UiState.Loading || isResignLoading) {
+            UiState.Loading
+        } else {
+            myInfo
+        }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, UiState.Loading)
 
     private val _uiEvent = MutableSharedFlow<UiEvent>()
     val uiEvent = _uiEvent.asSharedFlow()
@@ -42,20 +56,23 @@ class MypageViewModel @Inject constructor(
                     shopName = mypage.name,
                     status = mypage.status,
                 ).also {
-                    _uiState.emit(it)
+                    _myInfo.emit(it)
                 }
             }.onFailure {
-                _uiState.emit(UiState.Failure)
+                _myInfo.emit(UiState.Failure)
             }
         }
     }
 
     fun resign() {
         viewModelScope.launch {
+            _isResignLoading.emit(true)
             adminRepository.resign().onSuccess {
                 _uiEvent.emit(UiEvent.ResignSuccess)
             }.onFailure {
                 _uiEvent.emit(UiEvent.ResignFail)
+            }.onFinally {
+                _isResignLoading.emit(false)
             }
         }
     }
