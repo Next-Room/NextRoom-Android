f4061cd7778b7e0a92c89957589c50fff8563160
juhwankim-dev <juhwan.dev@gmail.com>
2025-01-16 22:28:13 +0900

    feat: 로그인 페이지에 문의하기 기능 추가

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/Constants.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/Constants.kt
@@ -2,4 +2,6 @@ package com.nextroom.nextroom.presentation.ui
 
 object Constants {
     const val MEMBERSHIP_PRODUCT = "membership_subscription"
+
+    const val KAKAO_BUSINESS_CHANNEL_URL = "http://pf.kakao.com/_mkBTn"
 }
