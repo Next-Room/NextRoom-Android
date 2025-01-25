526d9760e2df2376bc853d456d2cef7dd3333819
kyle.lee <tak8997@gmail.com>
2025-01-25 14:14:53 +0900

    feat: db converter 추가
    * 배경화면 커스텀 정보 converter 추가

--- data/src/main/java/com/nextroom/nextroom/data/db/TypeConverter.kt
+++ data/src/main/java/com/nextroom/nextroom/data/db/TypeConverter.kt
@@ -3,6 +3,7 @@ package com.nextroom.nextroom.data.db
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.nextroom.nextroom.domain.model.ThemeImageCustomInfo
 import java.lang.reflect.Type
 
 
@@ -26,5 +27,17 @@ class TypeConverter {
     @TypeConverter
     fun listToJson(list: List<String?>?): String {
         return Gson().toJson(list)
+    }
+
+    @TypeConverter
+    fun themeImageCustomInfoToJson(themeImageCustomInfo: ThemeImageCustomInfo?): String? {
+        if (themeImageCustomInfo == null) return null
+        return Gson().toJson(themeImageCustomInfo)
+    }
+
+    @TypeConverter
+    fun jsonToThemeImageCustomInfo(json: String?): ThemeImageCustomInfo? {
+        if (json == null) return null
+        return Gson().fromJson(json, ThemeImageCustomInfo::class.java)
     }
 }
