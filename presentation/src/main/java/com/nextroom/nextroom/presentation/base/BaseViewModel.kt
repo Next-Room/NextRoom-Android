7dce720c87a84ddaf2298225195aa71897007605
juhwankim-dev <juhwan.dev@gmail.com>
2025-02-26 01:06:17 +0900

    NR-104 exception을 핸들링하는 base scope 구현
    왜?
    앱 전반적으로 exception 발생시 처리가 미흡한 부분들이 있음
    viewModelScope 대신 baseViewModelScope를 사용하여 공통으로 처리하도록 함
    
    어떻게?
    이번 커밋에서는 일단 앱이 죽지 않게 하고, 로그를 남기는 것까지만 작업함

--- presentation/src/main/java/com/nextroom/nextroom/presentation/base/BaseViewModel.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/base/BaseViewModel.kt
@@ -1,6 +1,11 @@
 package com.nextroom.nextroom.presentation.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nextroom.nextroom.presentation.BuildConfig
+import com.nextroom.nextroom.presentation.util.Logger
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.plus
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import timber.log.Timber
@@ -9,6 +14,17 @@ abstract class BaseViewModel<STATE : Any, SIDE_EFFECT : Any> :
     ContainerHost<STATE, SIDE_EFFECT>,
     ViewModel() {
     abstract override val container: Container<STATE, SIDE_EFFECT>
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        handleError(throwable)
+    }
+
+    protected val baseViewModelScope = viewModelScope + exceptionHandler
+
+    private fun handleError(throwable: Throwable) {
+        if (!BuildConfig.DEBUG) {
+            Logger.e("${this::class.simpleName} generated\n${throwable.message}")
+        }
+    }
 
     init {
         Timber.tag("MANGBAAM-BaseViewModel()").d("${this::class.simpleName} generated")
