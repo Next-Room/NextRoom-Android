616e6e4b094d9ae7bc9f43caf355a2bae5c5e602
juhwankim-dev <juhwan.dev@gmail.com>
2025-03-02 15:00:44 +0900

    NR-105 사용하지 않는 파일 삭제
    왜?
    카운트 화면은 더이상 사용하지 않으므로 삭제함

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/counter/CounterState.kt
+++
@@ -1,8 +0,0 @@
-package com.nextroom.nextroom.presentation.ui.counter
-
-import com.nextroom.nextroom.domain.model.TimerState
-
-data class CounterState(
-    val lastSeconds: Int = 3,
-    val timerState: TimerState = TimerState.Ready,
-)
