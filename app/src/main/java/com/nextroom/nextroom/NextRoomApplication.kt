67f26c13786e0e99ad86851a6ff114155de4f39c
juhwankim-dev <juhwan.dev@gmail.com>
2025-03-03 20:20:36 +0900

    NR-106 flipper 기능 추가

--- app/src/main/java/com/nextroom/nextroom/NextRoomApplication.kt
+++ app/src/main/java/com/nextroom/nextroom/NextRoomApplication.kt
@@ -1,15 +1,46 @@
 package com.nextroom.nextroom
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class NextRoomApplication : Application() {
+    @Inject
+    lateinit var flipperNetworkPlugin: NetworkFlipperPlugin
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+
+        setDebugTool()
+    }
+
+    private fun setDebugTool() {
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(
+                InspectorFlipperPlugin(
+                    applicationContext,
+                    DescriptorMapping.withDefaults(),
+                ),
+            )
+            client.addPlugin(SharedPreferencesFlipperPlugin(this, "app-settings.json"))
+            client.addPlugin(flipperNetworkPlugin)
+            client.start()
         }
     }
 }
