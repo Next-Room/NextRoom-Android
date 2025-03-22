4a7781de9b7476c19ba6e1042cb23e9d727bba70
juhwankim-dev <juhwan.dev@gmail.com>
2025-03-22 23:15:44 +0900

    NR-110 로그인 페이지 디자인 변경 적용
    새로운 디자인에 맞게 변경함!

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/LoginFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/LoginFragment.kt
@@ -1,33 +1,10 @@
 package com.nextroom.nextroom.presentation.ui.onboarding
 
-import android.os.Bundle
-import android.view.View
-import androidx.navigation.fragment.findNavController
-import com.nextroom.nextroom.presentation.R
 import com.nextroom.nextroom.presentation.base.BaseFragment
 import com.nextroom.nextroom.presentation.databinding.FragmentLoginBinding
-import com.nextroom.nextroom.presentation.extension.safeNavigate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initViews()
-    }
-
-    private fun initViews() {
-        binding.btnSignUp.setOnClickListener {
-            val action = LoginFragmentDirections.moveToWebViewFragment(
-                url = getString(R.string.link_signup),
-                showToolbar = true,
-            )
-            findNavController().safeNavigate(action)
-        }
-        binding.tvExistingUserGuide.setOnClickListener {
-            findNavController().popBackStack()
-        }
-    }
 }
