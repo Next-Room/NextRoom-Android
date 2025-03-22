53946f2f49779f5fe3ba3c02fbd17b28310a6c91
juhwankim-dev <juhwan.dev@gmail.com>
2025-03-23 00:17:22 +0900

    NR-110 이메일로 시작하기 클릭시 동작 구현

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/LoginFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/LoginFragment.kt
@@ -1,10 +1,30 @@
 package com.nextroom.nextroom.presentation.ui.onboarding
 
-import com.nextroom.nextroom.presentation.base.BaseFragment
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.nextroom.nextroom.presentation.base.BaseViewModelFragment
 import com.nextroom.nextroom.presentation.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+class LoginFragment : BaseViewModelFragment<FragmentLoginBinding, LoginViewModel>(FragmentLoginBinding::inflate),
+    View.OnClickListener {
+    override val screenName = "login"
+    override val viewModel: LoginViewModel by viewModels()
 
+    override fun initListeners() {
+        super.initListeners()
+        binding.tvStartWithEmail.setOnClickListener(this)
+    }
+
+    private fun moveToEmailLogin() {
+        LoginFragmentDirections.moveToEmailLogin().also { findNavController().navigate(it) }
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            binding.tvStartWithEmail -> moveToEmailLogin()
+        }
+    }
 }
---
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/onboarding/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.nextroom.nextroom.presentation.ui.onboarding
+
+import com.nextroom.nextroom.presentation.base.NewBaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+
+) : NewBaseViewModel() {
+    // TODO: 구현 예정
+}
