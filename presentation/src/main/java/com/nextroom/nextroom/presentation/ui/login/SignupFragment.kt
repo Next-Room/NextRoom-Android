68a6c8dafd700a64dd7e86c38178e3b5a6ce8b55
juhwankim-dev <juhwan.dev@gmail.com>
2025-03-30 18:28:19 +0900

    NR-110 가입 경로 선택시 로직 구현
    어떻게?
    가입 경로 선택 뷰를 클릭하면 바텀시트를 띄우고
    선택한 값을 저장하여 화면에 반영하는 로직을 구현함!

--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/login/SignupFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/login/SignupFragment.kt
@@ -1,10 +1,23 @@
 package com.nextroom.nextroom.presentation.ui.login
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.BundleCompat
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.nextroom.nextroom.presentation.NavGraphDirections
+import com.nextroom.nextroom.presentation.R
 import com.nextroom.nextroom.presentation.base.BaseViewModelFragment
 import com.nextroom.nextroom.presentation.databinding.FragmentSignupBinding
+import com.nextroom.nextroom.presentation.extension.BUNDLE_KEY_RESULT_DATA
+import com.nextroom.nextroom.presentation.extension.hasResultData
+import com.nextroom.nextroom.presentation.extension.repeatOnStarted
+import com.nextroom.nextroom.presentation.model.SelectItemBottomSheetArg
+import kotlinx.coroutines.launch
 
-class SignupFragment : BaseViewModelFragment<FragmentSignupBinding, SignupViewModel>(FragmentSignupBinding::inflate) {
+class SignupFragment : BaseViewModelFragment<FragmentSignupBinding, SignupViewModel>(FragmentSignupBinding::inflate),
+    View.OnClickListener {
     override val screenName = "signup"
     override val viewModel: SignupViewModel by viewModels()
 
@@ -12,5 +25,87 @@ class SignupFragment : BaseViewModelFragment<FragmentSignupBinding, SignupViewMo
         super.initViews()
 
         binding.tvSignupComplete.isEnabled = false
+    }
+
+    override fun initListeners() {
+        super.initListeners()
+
+        binding.llSignupSource.setOnClickListener(this)
+        binding.llSignupReason.setOnClickListener(this)
+    }
+
+    override fun setFragmentResultListeners() {
+        super.setFragmentResultListeners()
+
+        setFragmentResultListener(SELECT_SIGNUP_SOURCE_REQUEST_KEY, ::handleFragmentResults)
+    }
+
+    private fun handleFragmentResults(requestKey: String, bundle: Bundle) {
+        when (requestKey) {
+            SELECT_SIGNUP_SOURCE_REQUEST_KEY -> {
+                if (bundle.hasResultData()) {
+                    BundleCompat.getParcelable(
+                        bundle,
+                        BUNDLE_KEY_RESULT_DATA,
+                        SelectItemBottomSheetArg.Item::class.java
+                    )
+                        ?.let { SignupViewModel.UIState.Loaded.SelectedItem(id = it.id, text = it.text) }
+                        ?.let { viewModel.setSelectedSignupSource(it) }
+                }
+            }
+        }
+    }
+
+    override fun initObserve() {
+        super.initObserve()
+
+        viewLifecycleOwner.repeatOnStarted {
+            launch {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is SignupViewModel.UIState.Loaded -> updateUI(state)
+                        SignupViewModel.UIState.Loading -> Unit // TODO: 수정
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateUI(state: SignupViewModel.UIState.Loaded) {
+        state.selectedSignupSource?.let {
+            binding.tvSignupSource.text = it.text
+        }
+    }
+
+    private fun showSelectSignupSourceBottomSheet(selectedItem: SignupViewModel.UIState.Loaded.SelectedItem? = null) {
+        resources.getStringArray(R.array.signup_source).mapIndexed { index, s ->
+            SelectItemBottomSheetArg.Item(
+                id = index.toString(),
+                text = s,
+                isSelected = index == selectedItem?.id?.toIntOrNull()
+            )
+        }.let {
+            SelectItemBottomSheetArg(
+                header = getString(R.string.text_select_signup_source),
+                items = it,
+                requestKey = SELECT_SIGNUP_SOURCE_REQUEST_KEY
+            )
+        }.also {
+            findNavController().navigate(NavGraphDirections.moveToSelectItem(it))
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            binding.llSignupSource -> {
+                (viewModel.uiState.value as? SignupViewModel.UIState.Loaded)?.let { loaded ->
+                    showSelectSignupSourceBottomSheet(loaded.selectedSignupSource)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val SELECT_SIGNUP_SOURCE_REQUEST_KEY = "SELECT_SIGNUP_SOURCE_REQUEST_KEY"
     }
 }
--- presentation/src/main/java/com/nextroom/nextroom/presentation/ui/login/SignupViewModel.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/ui/login/SignupViewModel.kt
@@ -2,11 +2,41 @@ package com.nextroom.nextroom.presentation.ui.login
 
 import com.nextroom.nextroom.presentation.base.NewBaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class SignupViewModel @Inject constructor(
 
 ) : NewBaseViewModel() {
-    // TODO: 구현 예정
+    private val _selectedSignupSource = MutableStateFlow<UIState.Loaded.SelectedItem?>(null)
+    private val _selectedSignupReason = MutableStateFlow<UIState.Loaded.SelectedItem?>(null)
+
+    val uiState = combine(
+        _selectedSignupSource,
+        _selectedSignupReason
+    ) { selectedSignupSource, selectedSignupReason ->
+        UIState.Loaded(selectedSignupSource, selectedSignupReason)
+    }.stateIn(baseViewModelScope, SharingStarted.Lazily, UIState.Loading)
+
+    fun setSelectedSignupSource(selectedItem: UIState.Loaded.SelectedItem) {
+        _selectedSignupSource.update { selectedItem }
+    }
+
+    sealed interface UIState {
+        data object Loading : UIState
+        data class Loaded(
+            val selectedSignupSource: SelectedItem?,
+            val selectedSignupReason: SelectedItem?,
+        ) : UIState {
+            data class SelectedItem(
+                val id: String,
+                val text: String,
+            )
+        }
+    }
 }
