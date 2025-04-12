2683183986b9db4d6b08ab5d0c7b3f0e3e497f28
juhwankim-dev <juhwan.dev@gmail.com>
2025-04-13 01:01:32 +0900

    BaseViewModelFragment에서 Error 수집시 one button dialog를 띄운다.
    왜?
    oneButtonDialog에 에러 내용을 볼 수 있는 기능을 넣어두었기 때문에
    에러 파악을 용이하게 하기 위해 토스트 대신 다이얼로그를 띄우도록 변경함

--- presentation/src/main/java/com/nextroom/nextroom/presentation/base/BaseViewModelFragment.kt
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/base/BaseViewModelFragment.kt
@@ -1,9 +1,12 @@
 package com.nextroom.nextroom.presentation.base
 
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
+import com.nextroom.nextroom.presentation.NavGraphDirections
 import com.nextroom.nextroom.presentation.R
+import com.nextroom.nextroom.presentation.common.NROneButtonDialog
 import com.nextroom.nextroom.presentation.extension.repeatOnStarted
-import com.nextroom.nextroom.presentation.extension.toast
+import com.nextroom.nextroom.presentation.extension.safeNavigate
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModelFragment<VB : ViewBinding, VM : NewBaseViewModel>(private val inflate: Inflate<VB>) :
@@ -16,8 +19,14 @@ abstract class BaseViewModelFragment<VB : ViewBinding, VM : NewBaseViewModel>(pr
         viewLifecycleOwner.repeatOnStarted {
             launch {
                 viewModel.errorFlow.collect {
-                    // TODO: OneButtonDialog 제작 후 수정
-                    toast(R.string.error_something)
+                    NavGraphDirections.moveToNrOneButtonDialog(
+                        NROneButtonDialog.NROneButtonArgument(
+                            title = getString(R.string.dialog_noti),
+                            message = getString(R.string.error_something),
+                            btnText = getString(R.string.text_confirm),
+                            errorText = it.message,
+                        )
+                    ).also { findNavController().safeNavigate(it) }
                 }
             }
         }
