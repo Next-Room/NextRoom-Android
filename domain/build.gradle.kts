75caab8577a95082f9d8739d2afce22573df02ad
juhwankim-dev <juhwan.dev@gmail.com>
2025-11-14 22:11:38 +0900

    jvmToolchain 값 설정

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,6 +9,10 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation(libs.javax.inject)
     implementation(libs.kotlinx.coroutines.core)
