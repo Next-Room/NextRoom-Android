5156ea92b286faf2094510e2dfdab45e401e05d5
juhwankim-dev <juhwan.dev@gmail.com>
2025-12-28 01:58:20 +0900

    NR-128 compose 공통 컴포넌트(로딩, 툴바) 구현

---
+++ presentation/src/main/java/com/nextroom/nextroom/presentation/common/compose/NRToolbar.kt
@@ -0,0 +1,83 @@
+package com.nextroom.nextroom.presentation.common.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nextroom.nextroom.presentation.R
+import com.nextroom.nextroom.presentation.extension.throttleClick
+
+@Composable
+fun NRToolbar(
+    title: String,
+    onBackClick: () -> Unit,
+    onRightButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = title,
+            style = NRTypo.Poppins.size20,
+            color = NRColor.White,
+            textAlign = TextAlign.Center,
+        )
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(64.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_navigate_back_24),
+                modifier = modifier
+                    .size(64.dp)
+                    .throttleClick { onBackClick() }
+                    .padding(20.dp),
+                contentDescription = null,
+            )
+
+            Text(
+                text = stringResource(R.string.memo_button),
+                color = NRColor.Dark01,
+                style = NRTypo.Poppins.size14,
+                modifier = modifier
+                    .padding(end = 20.dp)
+                    .background(
+                        color = NRColor.White,
+                        shape = RoundedCornerShape(size = 50.dp)
+                    )
+                    .padding(vertical = 6.dp, horizontal = 16.dp)
+                    .throttleClick { onRightButtonClick() }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF1D1B20)
+@Composable
+private fun NRToolbarPreview() {
+    NRToolbar(
+        title = "01:23:45",
+        onBackClick = {},
+        onRightButtonClick = {}
+    )
+}
